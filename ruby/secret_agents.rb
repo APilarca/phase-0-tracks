19969f38e06ceac81c54c5a53b5913cfbb0a4761
Angelica Pilarca <angelpill15@gmail.com>
2016-07-18 00:29:40 -0700

    first draft

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -9,18 +9,19 @@
     # subtract 1 to index
     # return that letter(value)
 
-
-
-until times_run == input.length
-
 times_run = 0
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 input = "abc"
-new_password = ""
-    new_value = 0
 
-    new_value = alphabet.index(input[times_run]) + 1
-    new_password += alphabet[new_value]
-    times_run += 1
+
+ until times_run == input.length
+        new_value = alphabet.index(input[times_run]) + 1
+            if new_value > 25
+                new_value = new_value - 26
+            end
+        new_password += alphabet[new_value]
+        times_run += 1
+
+    end
+    puts "Your encrypted password is #{new_password}."
 end
-puts new_password

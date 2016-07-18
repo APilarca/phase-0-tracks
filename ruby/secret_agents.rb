8aa8ad60db85ecd9259daade2bf646f6fe2949a7
Angelica Pilarca <angelpill15@gmail.com>
2016-07-18 00:31:42 -0700

    modifies a little bit

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,20 +1,10 @@
-# encrypt - moves one letter forward
-    # determine input length for loop number
-    # search alphabet string for index.
-    # add 1 to index
-    # return that letter(value)
+def encrypt(input)
+    new_value = 0
+    new_password = ""
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    times_run = 0
 
-# decrypt - moves one letter backwards
-    # search alphabet string for index.
-    # subtract 1 to index
-    # return that letter(value)
-
-times_run = 0
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-input = "abc"
-
-
- until times_run == input.length
+    until times_run == input.length
         new_value = alphabet.index(input[times_run]) + 1
             if new_value > 25
                 new_value = new_value - 26
@@ -26,4 +16,21 @@ input = "abc"
     puts "Your encrypted password is #{new_password}."
 end
 
-encrypt(decrypt(swordfish))
+
+def decrypt(input)
+    new_value = 0
+    new_password = ""
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    times_run = 0
+
+    until times_run == input.length
+        new_value = alphabet.index(input[times_run]) - 1
+            if new_value < 0
+                new_value = new_value + 26
+            end
+        new_password += alphabet[new_value]
+        times_run += 1
+
+    end
+    puts "Your password is #{new_password}."
+end

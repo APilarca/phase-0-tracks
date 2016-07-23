518d614b0a772daaa3a045f8091a0736bcdf0840
Angelica Pilarca <angelpill15@gmail.com>
2016-07-23 16:50:03 -0700

    creates interior design application outline

---
+++ ruby/hashes.rb
@@ -0,0 +1,23 @@
+#prompt user to enter information
+#use symbols for itemss in hash
+#convert data to appropriate type
+
+
+interior_design {
+	client_name: ,
+	age: ,
+	children: ,
+	theme: ,
+	time_allowed:
+}
+
+puts "Please enter your full name: "
+:name = gets.chomp
+puts "Please enter your age: "
+:age = gets.chomp.to_i
+puts "Do you have children: "
+:children = gets.chomp.to_b
+puts "Please enter your desired theme: "
+:theme = gets.chomp
+puts "Please enter the time frame you need your project completed in: "
+:time = gets.chomp_i

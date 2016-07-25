a67c3881966636fa43586e7d287b064504ea9361
Angelica Pilarca <angelpill15@gmail.com>
2016-07-24 21:38:45 -0700

    finishes creating a short design application

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,23 +1,43 @@
-#prompt user to enter information
-#use symbols for itemss in hash
-#convert data to appropriate type
+#create a hash for interior design
+#log information into hash based on clients answers
 
 
-interior_design = {
-	:client_name=> "",
-	:age=>"" ,
-	:children=>"" ,
-	:theme=>"" ,
-	:time_allowed=>""
-}
 
-puts "Please enter your full name: "
-:name = gets.chomp
-puts "Please enter your age: "
-:age = gets.chomp.to_i
-puts "Do you have children: "
-:children = gets.chomp.to_b
+def interior_design(project_name)
+	project_name = {
+		client_name:nil,
+		age:nil,
+		number_of_children:nil,
+		theme:nil,
+		pay_cash:nil,
+	
+	}
+
+puts "Please enter your name: "
+project_name[:client_name] = gets.chomp
+puts "What is your age: "
+project_name[:age] = gets.chomp.to_i
+puts "Please enter your number of children: "
+project_name[:number_of_children] = gets.chomp.to_i
 puts "Please enter your desired theme: "
-:theme = gets.chomp
-puts "Please enter the time frame you need your project completed in: "
-:time = gets.chomp_i
+project_name[:theme] = gets.chomp
+puts "wil you pay cash?"
+project_name[:pay_cash] = gets.chomp
+p project_name
+puts "Is there any information you would like to update? If yes please type name of category you wish to update, if not please type 'none'"
+answer = gets.chomp
+if answer == "none"
+	puts "Thank you for filling out this information!"
+else
+	puts "What is the updated information for this category?"
+	project_name[answer.to_sym] = gets.chomp
+	p project_name
+end
+end
+#beginning of user interaction
+puts "What would you like your project name to be?"
+name = gets.chomp
+
+interior_design(name)
+
+
--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -16,18 +16,17 @@
 "Stop! You’re under arrest!".upcase
  => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".
-#=> "the usual suspects"
+ "the usual"<<" suspects"
+=> "the usual suspects"
 
-# " suspects".<???>
-# => "the usual suspects"
+ " suspects".prepend("the usual")
+ => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+ "The case of the disappearing last letter".chop
+ => "The case of the disappearing last lette"
 
 # "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
-
 # "Elementary,    my   dear        Watson!".<???>
 # => "Elementary, my dear Watson!"
 

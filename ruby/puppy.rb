dd18c293082b432f5c2d382c7141418e42eebbbb
Angelica Pilarca <angelpill15@gmail.com>
2016-07-26 18:04:32 -0700

    creates an idea of the puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+'''
+SPECIES-----------
+
+CHARACTERISTICS-----------
+Eyes: purple
+Fur: green and pink spots
+Legs: 6
+name: Spot or Scraggles
+tails: 2
+
+BEHAVIOR----------
+Lick
+Nuzzle
+Nibble
+Bark
+'''

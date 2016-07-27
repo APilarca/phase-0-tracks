aaa56482c89932b353074dc02be484a7b26572d5
Angelica Pilarca <angelpill15@gmail.com>
2016-07-26 21:39:46 -0700

    finishes release 0 and teaches sprinkles new tricks

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,32 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+  
+  def speak(times)
+  	puts "Bark!" * times
+  end
+  
+  def roll_over
+  	puts "rolls over"
+  end
+  
+  def dog_years(age)
+  	age = age * 7
+  	puts "Age in dogs years is #{age}"
+  end
+  
+  def play_dead
+  	puts "Falls to floor with tongue hanging out"
+  end
 
 end
+ball = "ball"
+times = 3
 
+sprinkles = Puppy.new
+
+sprinkles.fetch(ball)
+sprinkles.speak(3)
+sprinkles.roll_over
+sprinkles.dog_years(3)
+sprinkles.play_dead

772de1c62ea21b41680fb7946afcacdeacb469c3
Angelica Pilarca <angelpill15@gmail.com>
2016-07-27 17:32:24 -0700

    expalins to user the different methods santa can use

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,16 +1,21 @@
 class Santa
+	#prints message when new isntance of santa
+	#is created
 	def initialize
 		puts "Initializing Santa instance..."
 	end
+	#enables out santa to spread holiday cheer
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
-
+	#allows santa to compliment sustenance
+	#given to him
 	def eat_milk_and_cookies(cookie_type)
 		puts "That was a good #{cookie_type}!"
 	end
 end
 
+#driver code to test class methods
 nick = Santa.new
 nick.speak
 nick.eat_milk_and_cookies("snickerdoodle")

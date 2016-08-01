f1eebd135d1591d81e902456ccf06a3debf5598d
Angelica Pilarca <angelpill15@gmail.com>
2016-08-01 07:44:48 -0700

    create beginnings of user interface

--- ruby/solo/word_guess.rb
+++ ruby/solo/word_guess.rb
@@ -1,14 +1,17 @@
 class Game
+	attr_reader(:guess_count, :game_end)
 	def initialize(word)
 		@word = word
 		@guess_count = 0
 		@game_end = false
-		@guessed_letters = []
+		@guessed_letters = [ ]
+		@display = (@word.length).times.collect {"_"}.join(' ')
 	end
-	def guesses(word)
-		if word.length <= 5
+		
+	def guesses
+		if @word.length <= 5
 			@guess_count = 3
-		elsif word.length <= 10
+		elsif @word.length <= 10
 			@guess_count = 5
 		else
 			@guess_count = 8
@@ -16,21 +19,56 @@ class Game
 	end
 
 	def guess_tracker(letter)
+		arr = @word.split("")
+		arr.delete_at(0).delete_at(-1)
+		p arr
 		if @guessed_letters.include?(letter) == true
 			@guess_count
-		else
+			puts "You already guessed that!"
+			p @display
+		elsif
+			arr.index(letter) == nil
 			@guessed_letters.push(letter)
 			@guess_count -= 1
+			puts "sorry that isn't in the word"
+			p @display
+		else
+			num = arr.index(letter)
+			@display = @display.split
+			@display[num] = letter
+			p @display.join(' ')
 		end
 	end
 
 	def end_game
 		if guess_count == 0
+			puts "Aw, poor player ran out of guesses"
+			@game_end = true
+		elsif @display.delete(' ') == @word_to_guess
+			puts "Congratulations!!!"
 			@game_end = true
 		else
-			@game_end
+			@game_end = false
 		end
 	end
 
 
 end
+
+puts "Welcome to the word guessing game!"
+puts "Please enter a word you would like the player to guess: "
+word_to_guess = gets.chomp
+new_game = Game.new(word_to_guess)
+
+new_game.guesses
+puts "Okay player, you have #{new_game.guess_count} guesses to find the word"
+
+while new_game.game_end == false
+	puts "Please enter the letter you would like to guess"
+	guess = gets.chomp
+	new_game.guess_tracker(guess)
+	new_game.end_game
+end
+
+puts "Thank you for playing!!!"
+
--- ruby/solo/word_guess_spec.rb
+++ ruby/solo/word_guess_spec.rb
@@ -1,13 +1,14 @@
 require_relative 'word_guess'
 
 describe Game do
-	let(:word) {Game.new}
+	let(:word) {Game.new("what")}
 
 	it "determines how many guesses user gets" do
-		expect(word.guesses("happened")).to eq 5
+		expect(word.guesses).to eq 3
 	end
 
 	it "checks for repeated guesses, logs if guess is new, adds to guess count" do
-		expect(word.guess_tracker("a")).to eq ["a"]
+		expect(word.guess_tracker("a")).to eq "_ _ a _"
 	end
+
 end

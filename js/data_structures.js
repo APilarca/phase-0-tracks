841f5f47ac1cbdebbbf69cbb353867f60a6ed1f3
Angelica Pilarca <angelpill15@gmail.com>
2016-08-04 10:15:04 -0700

    fixed mistakes, reverts explore

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,2 +1,31 @@
-var colors = ["yellow_green", "sprinkle patterned", "chocolate", "black and blue tiger print"]
-var names = ["Ed", "Sugar", "Star", "Apple"]
+
+
+ var horse_names = ["Buster", "Sugar", "Ed", "Apple"];
+ var horse_colors = ["red", "blue", "green", "pink"];
+
+ horse_names.push("Johnny");
+ horse_colors.push("rainbow");
+
+ var horses = {};
+
+ for (var i = 0; i < horse_names.length; i++){
+   console.log(horses[i] = horse_names[i]);
+   console.log(horses.colors = horse_colors[i]);
+ }
+
+ console.log(horses)
+
+ for (var i = 0; i < horse_names.length; i++){
+  (horses[horse_names[i]] = horse_colors[i]); 
+ }
+
+console.log(horses)
+
+function Car(color, numberOfDoors, sunRoof) {
+ console.log("Our new car", this); 
+  this.color = color;
+  this.numberOfDoors = numberOfDoors;
+  this.sunRoof = sunRoof;
+  this.honk = function() {console.log("Honk!");
+                         };
+}

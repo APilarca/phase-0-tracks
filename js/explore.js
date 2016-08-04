bd999d6837b562f60537487e3ab4cd9ade9d95d1
Angelica Pilarca <angelpill15@gmail.com>
2016-08-04 10:09:50 -0700

    creates a js object and constructor

--- js/explore.js
+++ js/explore.js
@@ -1,19 +1,41 @@
-//take a string input by user
-//reverse the letters in the string
-//output the new string that is reversed
 
-function reverse(string) {
-	var original = string;
-	var newString = '';
-	for (i = (original.length - 1); i >= 0; i--) {
-		newString += original[i]
-	};
-	console.log(newString)
-};
 
-if (1 == 1) {
-	reverse("happy")
+ var horse_names = ["Buster", "Sugar", "Ed", "Apple"];
+ var horse_colors = ["red", "blue", "green", "pink"];
+
+ horse_names.push("Johnny");
+ horse_colors.push("rainbow");
+
+ var horses = {};
+
+ for (var i = 0; i < horse_names.length; i++){
+   console.log(horses[i] = horse_names[i]);
+   console.log(horses.colors = horse_colors[i]);
+ }
+
+ console.log(horses)
+
+ for (var i = 0; i < horse_names.length; i++){
+  (horses[horse_names[i]] = horse_colors[i]); 
+ }
+
+console.log(horses)
+
+function Car(color, numberOfDoors, sunRoof) {
+ console.log("Our new car", this); 
+  this.color = color;
+  this.numberOfDoors = numberOfDoors;
+  this.sunRoof = sunRoof;
+  this.honk = function() {console.log("Honk!");
+                         };
 }
-else {
-	null
-}
+
+var carOne = new Car("red", 2, true);
+console.log(carOne);
+carOne.honk();
+
+var carTwo = new Car("blue", 4, false);
+console.log(carTwo);
+
+var carThree = new Car("purple", 2, true);
+console.log(carThree);

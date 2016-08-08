14482e3e5a726453402fb8307f60e8088d6ccf4a
Angelica Pilarca <angelpill15@gmail.com>
2016-08-08 09:03:47 -0700

    creates driver code to test functions

--- js/algos.js
+++ js/algos.js
@@ -13,7 +13,7 @@ function longer(array) {
 function compare(obj1, obj2) {
 	var key = Object.keys(obj1);
 	var key2 = Object.keys(obj2);
-	for (i = 0; i < key.length; i ++) {
+	for (var i = 0; i < key.length; i ++) {
 	if (key[i] === key2[i]) {
 		if (obj1[key[i]] === obj2[key2[i]]) {
 			console.log("true");
@@ -26,6 +26,21 @@ function compare(obj1, obj2) {
 		console.log("false");
 	}
 };
+}
+
+function randData(length) {
+	newData = [];
+	alphabet = "abcdefghijklmnopqrstuvwxyz";
+	newString = ""
+	for (var i = 0; i < length; i++) {
+		for (var x = 0; x < 10; x++) {
+			newString += alphabet.charAt(Math.floor(Math.random() *alphabet.length));
+			
+		}
+		newData[i] = newString;
+	}
+
+	return newData;
 }
 
 
@@ -41,3 +56,12 @@ var second = {name: "A", age: 10};
 
 
 compare(first, second);
+randData(2);
+
+for (var n = 0; n < 10; n++) {
+	test = randData(2)
+	console.log(test);
+	longer(test);
+
+
+}

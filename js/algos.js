35798302a374d9e08cd4d8b65344a10196d1f74f
Angelica Pilarca <angelpill15@gmail.com>
2016-08-08 08:37:56 -0700

    creates a function to compare key value pairs

--- js/algos.js
+++ js/algos.js
@@ -11,7 +11,21 @@ function longer(array) {
 }
 
 function compare(obj1, obj2) {
-	
+	var key = Object.keys(obj1);
+	var key2 = Object.keys(obj2);
+	for (i = 0; i < key.length; i ++) {
+	if (key[i] === key2[i]) {
+		if (obj1[key[i]] === obj2[key2[i]]) {
+			console.log("true");
+			break;
+		} else {
+			console.log("false");
+		}
+		
+	} else {
+		console.log("false");
+	}
+};
 }
 
 
@@ -22,6 +36,8 @@ function compare(obj1, obj2) {
 // longer(["ahts", "and", "cats", "obviously the longest", "short", "obviously the longestsss"]);
 
 var first = {name: "A", age: 12};
-var second = {name: "B", age: 12};
+var second = {name: "A", age: 10};
+
+
 
 compare(first, second);
